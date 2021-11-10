6ca7209a83d25a7220ce41f7032c8233c79a86ab
shy228 <shy228@ucsd.edu>
2021-11-10 14:15:40 -0800

    update g_shsare

--- src/predictor.c
+++ src/predictor.c
@@ -34,9 +34,12 @@ int verbose;
 //------------------------------------//
 //
 //TODO: Add your own Branch Predictor data structures here
-//
+//the total history of the gshare
 uint32_t gshare_history;
+//the mapping table
 uint32_t *gs_table;
+//used to get the ghisotryBits index (lower)
+uint32_t gshare_mask;
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
@@ -52,9 +55,16 @@ void init_gshare()
   size = 1<<ghistoryBits;
   //each entry is either 0,1,2,3 -> only two bits, but the smallest we can have is 1 byte (8 bits).
   gs_table = malloc(sizeof(uint8_t) * size);
+  //each index (entry)
   for(unsigned int i = 0; i < size; i++){
     uint8_t mask = 0b0;
+    //initilize as weak taken (which is 1) by default
     gs_table[i] = mask | WN;
+  }
+  gshare_mask = 0;
+  //if ghistoryBits is 6 -> then we get 0b111111
+  for(int i = 0; i < ghistoryBits; i++){
+    gshare_mask = (gshare_mask << i) | 1;
   }
 }
 void
@@ -76,12 +86,19 @@ make_prediction(uint32_t pc)
   //
   //TODO: Implement prediction scheme
   //
-
+uint32_t actual_gshare_history = gshare_history & gshare_mask;
+uint32_t gshare_index = pc ^ actual_gshare_history;
+uint8_t gshare_prediction;
+uint8_t result = 0;
   // Make a prediction based on the bpType
   switch (bpType) {
     case STATIC:
       return TAKEN;
     case GSHARE:
+      gshare_prediction = gs_table[gshare_index];
+      //cast the 4 bytes integers to be 1 byte
+      if(gshare_prediction >= 2) return result | 1;
+      return result;
     case TOURNAMENT:
     case CUSTOM:
     default:
@@ -103,4 +120,7 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+  uint8_t result = make_prediction(pc);
+  uint32_t index = pc ^ (gshare_history & gshare_mask);
+  gshare_history = gshare_history << 1 | result;
 }
