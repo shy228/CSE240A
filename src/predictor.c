55cea392ed9b2524e600cb286dea0abb1a89ac86
shy228 <shy228@ucsd.edu>
2021-11-10 21:26:23 -0800

    gshare implemented

--- src/predictor.c
+++ src/predictor.c
@@ -55,25 +55,28 @@ void init_gshare()
   size = 1<<ghistoryBits;
   //each entry is either 0,1,2,3 -> only two bits, but the smallest we can have is 1 byte (8 bits).
   gs_table = malloc(sizeof(uint8_t) * size);
+  printf("size is: %d\n", size);
   //each index (entry)
   for(unsigned int i = 0; i < size; i++){
     uint8_t mask = 0b0;
     //initilize as weak taken (which is 1) by default
     gs_table[i] = mask | WN;
   }
+  printf("successfully assign value to gs_table\n");
   gshare_mask = 0;
   //if ghistoryBits is 6 -> then we get 0b111111
   for(int i = 0; i < ghistoryBits; i++){
-    gshare_mask = (gshare_mask << i) | 1;
+    gshare_mask = (gshare_mask << 1) | 1;
   }
 }
 void
 init_predictor()
 {
+  //printf("it runs here\n");
   if(bpType == GSHARE){
     init_gshare();
   }
-
+  //printf("gshare successfully initialized\n");
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -86,8 +89,9 @@ make_prediction(uint32_t pc)
   //
   //TODO: Implement prediction scheme
   //
-uint32_t actual_gshare_history = gshare_history & gshare_mask;
-uint32_t gshare_index = pc ^ actual_gshare_history;
+//printf("mask is: %d\n", gshare_mask);
+uint32_t gshare_index = (pc & gshare_mask) ^ (gshare_history & gshare_mask);
+//printf("index is: %d\n", gshare_index);
 uint8_t gshare_prediction;
 uint8_t result = 0;
   // Make a prediction based on the bpType
@@ -96,6 +100,7 @@ uint8_t result = 0;
       return TAKEN;
     case GSHARE:
       gshare_prediction = gs_table[gshare_index];
+      //printf("get the prediction\n");
       //cast the 4 bytes integers to be 1 byte
       if(gshare_prediction >= 2) return result | 1;
       return result;
@@ -120,7 +125,31 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
-  uint8_t result = make_prediction(pc);
-  uint32_t index = pc ^ (gshare_history & gshare_mask);
-  gshare_history = gshare_history << 1 | result;
+  //get the result
+  //uint8_t result = make_prediction(pc);
+  //get the index
+  uint32_t index = (pc & gshare_mask) ^ (gshare_history & gshare_mask);
+  //update the gshare_history
+  gshare_history = (gshare_history << 1) | outcome;
+  //update the gshare_table
+  uint8_t element = gs_table[index];
+  //there are 8 possible situations
+  if(element == SN){
+    //if the outcome is taken but our element of this entry is strongly nontaken
+    if(outcome == TAKEN) gs_table[index]++;
+  }
+  else if(element == WN){
+    //change to weak taken
+    if(outcome == TAKEN) gs_table[index]++;
+    //change to strongly not taken
+    else gs_table[index]--;
+  }
+  else if(element == WT){
+    if(outcome == TAKEN) gs_table[index]++;
+    else gs_table[index]--;
+  }
+  //this is the last case
+  else{
+    if(outcome == NOTTAKEN) gs_table[index]--;
+  }
 }
